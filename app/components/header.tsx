d85321b626b2937f5db63d00789f682c365253e3
Alex Stout <alex@Alex-Work-Mac.local>
2023-11-09 14:51:50 -0700

    feature/1 initial commit

---
+++ app/components/header.tsx
@@ -0,0 +1,13 @@
+
+export default function header() {
+  return (
+    <div className="flex justify-between align-middle bg-slate-500 w-full px-4 py-2">
+        <div>
+            Left Side
+        </div>
+        <div>
+            Right side
+        </div>
+    </div>
+  )
+}
