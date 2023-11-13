2057e60e3832f753700446d5037c0c7e09403683
Alex Stout <stizout@gmail.com>
2023-11-13 15:03:10 -0700

    finish up front end

--- app/(home)/try/page.tsx
+++ app/(home)/try/page.tsx
@@ -1,48 +1,86 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useRef, useState } from 'react';
 
-const defaultOptions = ['Mom', 'Mom', 'Mom', 'Mom', 'Mom'];
+const defaultOptions = ['Mom', 'Dad', 'Boss', 'Wife', 'Clergy'];
 
 export default function Try() {
   const [input, setInput] = useState('');
   const [translateTo, setTranslateTo] = useState('');
+  const [translateInput, setTranslateInput] = useState('');
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [filteredTranslate, setFilteredTranslate] = useState(defaultOptions);
+  const [prestige, setPrestige] = useState('Way longer than it should be just so i can test what this is going to look like');
 
   useEffect(() => {
-    if (translateTo.length > 0) {
-      const result = defaultOptions.filter((w) => w.toLowerCase().includes(translateTo.toLowerCase()));
+    document?.addEventListener('click', (e) => {
+      handleClick(e);
+    });
+    return () => {
+      document?.removeEventListener('click', () => {});
+    };
+  });
+
+  useEffect(() => {
+    if (translateInput.length > 0) {
+      const result = defaultOptions.filter((w) => w.toLowerCase().includes(translateInput.toLowerCase()));
       setFilteredTranslate(result);
     } else {
       setFilteredTranslate(defaultOptions);
     }
-  }, [translateTo]);
+  }, [translateInput]);
+
+  const handleClick = (e: MouseEvent): void => {
+    const path = e.composedPath();
+    const dropdown = path.includes(document.getElementById('dropdown') as Node);
+    const input = path.includes(document.getElementById('input') as Node);
+    if (input || dropdown) {
+      setDropdownOpen(true);
+    } else if (dropdownOpen && !dropdown) {
+      setDropdownOpen(false);
+      if (translateInput.length > 0) {
+        setTranslateTo(translateInput);
+      }
+    } else {
+      setDropdownOpen(false);
+    }
+  };
 
   const handleSubmit = () => {
-    alert('submit');
+    if (input.length > 0) {
+      alert('submit');
+    }
   };
+  const handleRowClick = (e: SetStateAction<string>) => {
+    setTranslateTo(e);
+    setTimeout(() => {
+      setDropdownOpen(false);
+    }, 50);
+  };
+
   return (
     <div className='flex flex-col items-center h-full w-full'>
       <div className='flex items-center h-full w-full'>
-        <div className='flex items-center flex-col w-1/2'>
+        <div className='flex items-center flex-col w-1/2 min-h-[100px]'>
           <span className='text-xl'>How you talk to your friends</span>
           <form onSubmit={() => handleSubmit()}>
-            <input value={input} onChange={(e) => setInput(e.target.value)} className='border-2 rounded mt-4 px-2 h-[30px] hover:bg-gray-200 hover:border-pink-200' />
+            <input value={input} onChange={(e) => setInput(e.target.value)} className='border-2 rounded mt-4 px-2 h-[30px] hover:bg-gray-200 hover:border-pink-200' onBlur={() => handleSubmit()} />
           </form>
         </div>
-        <div className='w-1/2 flex flex-col items-center'>
+        <div className='w-1/2 flex flex-col items-center min-h-[100px]'>
           {/* <span className='text-xl'>Translate to your wife</span> */}
-          <div className='text-xl rounded w-52 relative bg-white' onClick={() => setDropdownOpen(true)} onMouseLeave={() => setDropdownOpen(false)}>
-            <div>Here</div>
-            <div className='absolute rounded bg-white shadow-lg shadow-indigo-500/50 w-full' style={{ top: '25px', borderTop: 'none', borderTopRightRadius: '0', borderTopLeftRadius: '0', display: dropdownOpen ? 'block' : 'none' }}>
-              <input value={translateTo} onBlur={() => setDropdownOpen(false)} className='w-full border' style={{ borderLeft: '0', borderRight: '0' }} onChange={(e) => setTranslateTo(e.target.value)} />
+          <div id='input' className='text-xl rounded w-60 relative bg-white '>
+            <div className='px-2 text-center whitespace-nowrap'>{translateTo || 'Who are you talking to?'}</div>
+            <div id='dropdown' className='absolute rounded bg-white shadow-lg shadow-indigo-500/50 w-full' style={{ top: '25px', borderTop: 'none', borderTopRightRadius: '0', borderTopLeftRadius: '0', display: dropdownOpen ? 'block' : 'none' }}>
+              <input value={translateInput} className='w-full border' style={{ borderLeft: '0', borderRight: '0' }} onChange={(e) => setTranslateInput(e.target.value)} />
               {filteredTranslate.map((e, i) => (
-                <div key={i}>{e}</div>
+                <div className='hover:bg-gray-100 cursor-pointer' key={i} onClick={() => handleRowClick(e)}>
+                  {e}
+                </div>
               ))}
             </div>
           </div>
-          <p className='mt-4'>I acknowledge my mistake and take responsibility for it.</p>
+          <p className='mt-4 min-h-[30px]'>{prestige}</p>
         </div>
       </div>
       <div className='flex'>
